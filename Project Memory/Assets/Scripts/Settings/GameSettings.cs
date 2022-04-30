da31f21cd4f21f086c7d3b7b45957ce85db158a0
M I Y U <jessewestwesterlakenjesse@outlook.com>
2022-04-30 12:47:50 +0200

    added difficulty setting

--- Project Memory/Assets/Scripts/Settings/GameSettings.cs
+++ Project Memory/Assets/Scripts/Settings/GameSettings.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Game Settings", menuName = "ScriptableObjects/Game Settings", order = 2)]
 public class GameSettings : ScriptableObject
 {
+    public enum PlayerDiffuculty
+    {
+        easy,
+        normal,
+        hard
+    }
+
     [Header("Player Settings")]
     [Range(40, 120)] public float playerFOV = 70;
     public string playerName;
     public string playerVessel;
+    public PlayerDiffuculty diffuculty = PlayerDiffuculty.normal;
     public bool canDash;
 
     [Header("Game Settings")]
