308e96e9c8e26317060330ef988297ea7d844b68
M I Y U <jessewestwesterlakenjesse@outlook.com>
2023-02-11 19:06:46 +0100

    ConvertCurrency.cs work

--- Project Memory/Assets/Scripts/UI/ConvertCurrency.cs
+++ Project Memory/Assets/Scripts/UI/ConvertCurrency.cs
@@ -8,12 +8,18 @@ public class ConvertCurrency : MonoBehaviour
     //TO-DO: create a button/slider system for changing souls into currency
     //and changing the values of abilities with said currency
     [SerializeField] private InventoryData inventory;
+    [SerializeField] private float currencyValue = 0.25f;
 
-    private void TransferSoul()
+    public void TransferSoul()
     {
         if (inventory.soulAmount > 0)
         {
-
+            inventory.currency += (currencyValue * inventory.soulAmount);
+            inventory.soulAmount = 0;
+        }
+        else
+        {
+            // Make some txt appear
         }
     }
 }
---
+++ Project Memory/Assets/Scripts/UI/ShowDescription.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDescription : MonoBehaviour
+{
+    //TO-DO show an information box with item descriptions
+    //at the mouse origin point when hovering over said item
+    public void ActivateDescription(GameObject descriptionObj, string insertedText)
+    {
+
+    }
+}
